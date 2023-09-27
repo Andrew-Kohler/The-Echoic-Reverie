7ce585e2480996d8fb94c4f2ee008372591347b1
Andrew Kohler <awk480@gmail.com>
2023-09-27 13:44:21 -0400

    WIP - Adding a dev-friendly inspector panel to soundwave

---
+++ Assets/Scripts/SoundwaveController.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SoundwaveController : MonoBehaviour
+{
+    // Visual style
+    // Clean
+    // Messy
+    // STRANGE
+    enum LaunchConfigs{Clean, Messy, Strange};
+    
+
+    [Tooltip("Turns on live adjustments; to be used for testing new configs")]
+    [SerializeField] private bool experimentationMode;
+
+    [Header("General Settings")]
+    [Tooltip("Base particle system")]
+    [SerializeField] private ParticleSystem waveGenerator;
+    [Tooltip("Lifetime of the wave (seconds)")]
+    [SerializeField] private float lifetime = 2f;
+    [Tooltip("How much of the circle's circumference is used to send out the wave (0-360)")]
+    [SerializeField] private float emissionRadius = 90f;
+
+    [Header("Velocity Settings")]
+    [Tooltip("Initial velocity of the wave")]
+    [SerializeField] private float initialSpeed = 5f;            
+    [Tooltip("What % (0-1) of the particle velocity is lost on collision")]
+    [SerializeField] private float dampenRate = .2f;
+
+    [Header("Visual Settings")]
+    [Tooltip("Initial color of the wave")]
+    [SerializeField] private Color color = Color.blue;
+
+    [Header("Launch Configurations")]
+    [Tooltip("Different particle size and wave pattern presets")]
+    [SerializeField] private LaunchConfigs config;
+
+    private void Start()
+    {
+        UpdateParticleSettings();
+    }
+
+    private void Update()
+    {
+        if (experimentationMode)
+        {
+            UpdateParticleSettings(); 
+        }
+    }
+
+    private void UpdateParticleSettings()
+    {
+        //TODO: Swapping between two constants and one constant is Not Working
+        var shapeVariables = waveGenerator.shape;
+        var mainVariables = waveGenerator.main;
+        var mainVariablesSize = mainVariables.startSize;
+        var collisionVariables = waveGenerator.collision;
+        var noiseVariables = waveGenerator.noise;
+
+        shapeVariables.arc = this.emissionRadius;
+        mainVariables.startSpeed = this.initialSpeed;
+        collisionVariables.dampen = this.dampenRate;
+        mainVariables.startColor = this.color;
+
+        if (config == LaunchConfigs.Clean)
+        {
+            mainVariablesSize.mode = ParticleSystemCurveMode.Constant;
+            mainVariables.startSize = .2f;
+            noiseVariables.enabled = false;
+        }
+        else if (config == LaunchConfigs.Messy)
+        {
+            mainVariablesSize.mode = ParticleSystemCurveMode.TwoConstants;
+            mainVariablesSize.constantMin = .06f;
+            mainVariablesSize.constantMax = 1f;
+            noiseVariables.enabled = false;
+        }
+        else if (config == LaunchConfigs.Strange)
+        {
+            mainVariablesSize.mode = ParticleSystemCurveMode.TwoConstants;
+            mainVariablesSize.constantMin = .06f;
+            mainVariablesSize.constantMax = 1f;
+            noiseVariables.enabled = true;
+        }
+    }
+
+
+}
